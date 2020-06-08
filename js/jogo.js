6d633f449e42a3a8e4a629dd4d0b6cfdaf5dc546
pedrorreiro <pedro_rique17@hotmail.com>
2020-06-07 21:32:36 -0300

    versao final

--- js/jogo.js
+++ js/jogo.js
@@ -281,6 +281,8 @@ function salvaRecord(){
     salvaCookieTempo();
 
     atualizaRecord();
+
+    document.getElementById('record').style.display = "inline-block";
  
 }
 
@@ -288,6 +290,4 @@ function atualizaRecord(){
     document.getElementById('nomeRecord').innerHTML = getCookie('nome');
     document.getElementById('pontosRecord').innerHTML = getCookie('pontos');
     document.getElementById('tempo').innerHTML = getCookie('tempo');
-
-    document.getElementById('record').style.display = 'inline-block';
 }
